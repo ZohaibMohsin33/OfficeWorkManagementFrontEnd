77e4c8340740116cd960b1eb802341ddf66f9181
Muhammad Zohaib Mohsin <zohaibmohsin30@gmail.com>
2023-01-09 23:55:07 +0500

    links are added

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
 let activeStatus = (e)=>{
      removeActive();
      e.target.className += " active";
-     console.log(e.target);
-    //  Hello my name is apunich 
+
 }
 
 let removeActive = ()=>{
